75d20072228b529c89eeeab1d1456d4322ce03b5
Sebastian Waz <sebwaz@g.ucla.edu>
2017-12-23 15:39:48 -0800

    Revert "rand_drum.py random sequencer"
    This reverts commit eae0034acc3163268862cade3de6d968397de80b.

--- lib/code/audio/pyo/examples_tutorials/bplib_examples.py
+++ lib/code/audio/pyo/examples_tutorials/bplib_examples.py
@@ -1,9 +1,8 @@
 import bplib as bp
 
-'''
-example usage of bplib.sequencer
-'''
 snds_path = './pyo_examples/snds/'
 snds      = ['', snds_path+'e.aif', snds_path+'g#.aif', snds_path+'b.aif', snds_path+'a.aif']
 seq       = [1,2,1,2,1,2,3,1,2,1,2,1,2,3,4,2,4,2,4,2,3,4,2,4,2,4,2,3]
+
 bp.sequencer(snds, seq, 140, 2)
+
