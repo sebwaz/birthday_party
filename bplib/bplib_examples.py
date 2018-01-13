c5c7df5772472060b9e26bd59651336d39c73752
Sebastian Waz <sebwaz@g.ucla.edu>
2018-01-12 22:49:29 -0800

    made sequencer() function accept poly voice input
    vectors must be passed through to_seq() before being passed to sequencer

--- bplib/bplib_examples.py
+++ bplib/bplib_examples.py
@@ -3,15 +3,33 @@ import numpy as np
 
 example_number = int(input('Example to run: '))
 
+#################################
 if example_number == 1:
     '''
     1.) usage of bplib.sequencer
     '''
-    snds_path = './samples/sebas/aif/'
-    snds      = ['', snds_path+'e.aif', snds_path+'g#.aif', snds_path+'b.aif', snds_path+'a.aif']
-    seq       = [1,2,1,2,1,2,3,1,2,1,2,1,2,3,4,2,4,2,4,2,3,4,2,4,2,4,2,3]
+    # get sounds to play
+    snds_path  = './samples/sebas/aif/'
+    snds       = ['', snds_path+'e.aif', snds_path+'g#.aif', snds_path+'b.aif', snds_path+'a.aif', snds_path+'hat_charter.aif', snds_path+'bd_chicago.aif', snds_path+'Snaredrum.aif']
+
+    # create sequence for guitar samples
+    guitar     = bp.to_seq([1,2,1,2,1,2,3,1,2,1,2,1,2,3,4,2,4,2,4,2,3,4,2,4,2,4,2,3])
+
+    # create a hat sequence
+    hats       = bp.to_seq(np.tile([5,0], 14))
+
+    # create rest of drum sequence
+    kick_snare = bp.to_seq(np.tile([6,0,6,0,0,7,0], 4))
+
+    # merge them
+    seq = bp.merge_seq(guitar, hats)
+    seq = bp.merge_seq(seq, kick_snare)
+
+    # play
     bp.sequencer(snds, seq, 140, 2)
 
+
+#################################
 elif example_number == 2:
     '''
     2.) conversion of .wav file
@@ -20,6 +38,8 @@ elif example_number == 2:
     bp.convert_to_aif('./samples/OpenPathMusic44V1/sleighbells.wav', './samples/output/', 'TEST')
     print('Done.')
 
+
+#################################
 elif example_number == 3:
     '''
     3.) reading a .wav file as a waveform with values in range [-1, 1] 
